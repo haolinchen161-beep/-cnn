861284c53a1dd887fe3d098d637911388cf01055
haolinchen161-beep <haolinchen161@gmail.com>
2026-06-08 11:16:44 +0800

    Sync training package exports for graph workflow

--- training/__init__.py
+++ training/__init__.py
@@ -1,11 +1,18 @@
-"""Training package exports for MeshGraphNet FRF surrogate."""
+"""Training package exports for MeshGraphNet/GNN FRF surrogate."""
 
-from .trainer import train, evaluate
-from .losses import modal_loss, frf_loss
+from .trainer import train, evaluate, save_model
+from .losses import modal_loss, frf_loss, mac_loss, sign_aligned_mse, zeta_physics_loss
+from .augmentations import GraphBatchAugmenter, GeometryAugmenter
 
 __all__ = [
     "train",
     "evaluate",
+    "save_model",
     "modal_loss",
     "frf_loss",
+    "mac_loss",
+    "sign_aligned_mse",
+    "zeta_physics_loss",
+    "GraphBatchAugmenter",
+    "GeometryAugmenter",
 ]
