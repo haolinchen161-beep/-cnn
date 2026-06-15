e2d41b1148a8d4cc438d42188dfe453d9dc99982
haolinchen161-beep <haolinchen161@gmail.com>
2026-06-16 01:33:57 +0800

    Point model package to modal-only MeshGraphNet

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,1 @@
-"""Model package exports for FEM-aware MeshGraphNet modal surrogate."""
-
-from .frf_model import build_geometric_model
-from .meshgraphnet_frf_model import MeshGraphFRFModel
-from .physics_decoder import PhysicsDecoder
-
-__all__ = [
-    "build_geometric_model",
-    "MeshGraphFRFModel",
-    "PhysicsDecoder",
-]
+from .modal_meshgraphnet import MeshModalNet, build_geometric_model
