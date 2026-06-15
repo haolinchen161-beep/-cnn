d37ccebf99d3a40ecdd573f67c1bb06983bd511d
haolinchen161-beep <haolinchen161@gmail.com>
2026-06-15 16:44:59 +0800

    Set MeshGraphNet builder default node feature dim to 26

--- models/frf_model.py
+++ models/frf_model.py
@@ -3,8 +3,15 @@
 from .meshgraphnet_frf_model import MeshGraphFRFModel
 
 
+DEFAULT_NODE_FEATURE_DIM = 26
+DEFAULT_EDGE_FEATURE_DIM = 4
+
+
 def build_geometric_model(encoder_kwargs=None, decoder_kwargs=None):
-    kwargs = {}
+    kwargs = {
+        "node_in_dim": DEFAULT_NODE_FEATURE_DIM,
+        "edge_in_dim": DEFAULT_EDGE_FEATURE_DIM,
+    }
     kwargs.update(encoder_kwargs or {})
     kwargs.update(decoder_kwargs or {})
     return MeshGraphFRFModel(**kwargs)
