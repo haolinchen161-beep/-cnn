bf3afd6b8370f24adda81f7e8ee994b3f355f419
haolinchen161-beep <haolinchen161@gmail.com>
2026-06-16 12:49:50 +0800

    Simplify training package exports

--- training/__init__.py
+++ training/__init__.py
@@ -1,9 +1,5 @@
-from .modal_losses_scaled import frequency_loss, modal_loss, weighted_phi_z_terms
-from .modal_trainer_simple import evaluate_modal, run_epoch, train_modal
-
-# Backward-compatible aliases for older sample scripts.
-train = train_modal
-evaluate = evaluate_modal
+from .modal_losses import frequency_loss, modal_loss, weighted_phi_z_terms
+from .modal_trainer import evaluate_modal, run_epoch, train_modal
 
 __all__ = [
     "frequency_loss",
@@ -12,6 +8,4 @@ __all__ = [
     "train_modal",
     "evaluate_modal",
     "run_epoch",
-    "train",
-    "evaluate",
 ]
